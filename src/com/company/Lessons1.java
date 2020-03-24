33f7fd8a58c85478316fcb859f8f1b0dcc144847
andrei <andrew.a.rubin@gmail.com>
2020-03-24 19:08:23 +0300

    first commit

---
+++ src/com/company/Lessons1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Lessons1 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("rubin_Andrei");
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+
+    //psvm
+    //sout
+    // Ctrl + j
+    public static void main(String[] args) {
+      //  int a ;
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        double q = 0.5;
+
+        //System.out.println(q++);
+        System.out.println(++q);
+    }
+
+}
